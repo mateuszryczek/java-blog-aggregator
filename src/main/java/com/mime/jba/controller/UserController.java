756e01707d96fbf1fcca3000101e36250a88f0ae
Mateusz Ryczek <mateuszryczek@yahoo.com>
2016-06-20 20:14:40 +0200

    Blog listing setup

--- src/main/java/com/mime/jba/controller/UserController.java
+++ src/main/java/com/mime/jba/controller/UserController.java
@@ -10,7 +10,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.mime.jba.entity.Blog;
 import com.mime.jba.entity.User;
+import com.mime.jba.service.BlogService;
 import com.mime.jba.service.UserService;
 
 @Controller
@@ -19,9 +21,17 @@ public class UserController {
 	@Autowired
 	private UserService userService;
 	
+	@Autowired
+	private BlogService blogService;
+	
 	@ModelAttribute("user")
-	public User construct(){
+	public User constructUser(){
 		return new User();
+	}
+	
+	@ModelAttribute("blog")
+	public Blog constructBlog(){
+		return new Blog();
 	}
 	
 	@RequestMapping("/users")
@@ -52,6 +62,13 @@ public class UserController {
 		String name = principal.getName();
 		model.addAttribute("user", userService.findOneWithBlogs(name));
 		return "user-detail";
+	}
+	
+	@RequestMapping(value="/account", method=RequestMethod.POST)
+	public String doAddBlog(@ModelAttribute("blog") Blog blog, Principal principal){
+		String name = principal.getName();
+		blogService.save(blog, name);
+		return "redirect:/account.html";
 	}
 	
 }
---
+++ src/main/java/com/mime/jba/service/BlogService.java
@@ -0,0 +1,26 @@
+package com.mime.jba.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mime.jba.entity.Blog;
+import com.mime.jba.entity.User;
+import com.mime.jba.repository.BlogRepository;
+import com.mime.jba.repository.UserRepository;
+
+@Service
+public class BlogService {
+	
+	@Autowired
+	private BlogRepository blogRepository;
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	public void save(Blog blog, String name){
+		User user = userRepository.findByName(name);
+		blog.setUser(user);
+		blogRepository.save(blog);
+	}
+
+}
